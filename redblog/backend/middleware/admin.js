126a7642e0ce006d2154bd644a955572b33d93c6
jose1-utp <jose.orozco1@utp.edu.co>
2025-11-28 21:29:48 -0500

    eliminado

--- redblog/backend/middleware/admin.js
+++
@@ -1,7 +0,0 @@
-// Middleware para verificar si el usuario es admin
-module.exports = function (req, res, next) {
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Acceso denegado. Se requiere rol de administrador.' });
-  }
-  next();
-};
